20b26723e759c636c4318d86146d9588c9a444e7
T Fiat <00324886@rijnijssel.nl>
2024-06-05 09:43:14 +0200

    Company card update

--- resources/views/components/company-card.blade.php
+++ resources/views/components/company-card.blade.php
@@ -1,12 +1,17 @@
 @props(['company'])
-
-<div class="h-32 p-3 bg-gray-300 rounded-md flex flex-col w-96">
-    <h2 class="text-xl font-semibold">
-        <a target="_blank">{{ $company->name }}</a>
-    </h2>
+<div class="h-32 p-3 bg-gray-300 rounded-md flex flex-col w-full">
+    <div class="flex items-center"> <!-- Container for company logo and name -->
+        <img class="w-24 h-24 rounded-full border-2 border-gray-500" src="{{ $company->logo_url }}" alt="{{ $company->name }} Logo">
+        <h2 class="text-xl font-semibold ml-3">
+            <a target="_blank">{{ $company->name }}</a>
+        </h2>
+    </div>
     <p class="text-sm">{{ $company->description }}</p>
-
-    <div class="mt-auto flex flex-row justify-between">
-        <a class="self-end hover:underline" href="/companies/{{ $company->id }}">Click here to see more</a>
+    <div class="flex justify-end"> <!-- Aligns the link to the right -->
+        <div class="mb-4"> <!-- Apply margin to this div -->
+            <a class="self-end hover:underline" href="/companies/{{ $company->id }}">Click here to see more</a>
+        </div>
     </div>
 </div>
+
+
